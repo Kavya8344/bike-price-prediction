229f929e4a9ec52795eef5aa7d282350424b5fff
Kavya Sharma <kavysharma2004@gmail.com>
2026-06-23 14:05:44 +0530

    Extracted model and Connected backend

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, flash, redirect, url_for
+import joblib
+import json
 
 app = Flask(__name__)
+
+model=joblib.load("model/lr.lb")
 
 @app.route('/')
 def index():
@@ -10,6 +14,21 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/project',methods=['GET','POST'])
+def predict():
+    if request.method=="POST":
+        data=request.get_json()
+    
+    return render_template('project.html')
 
 if __name__ == "__main__":
     app.run()
