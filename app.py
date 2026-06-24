2f1f93c8b2fa81b9e9ab2444e0fdba48d48b7531
Kavya Sharma <kavysharma2004@gmail.com>
2026-06-24 22:48:42 +0530

    Modified app.py and requirments.txt

--- app.py
+++ app.py
@@ -25,10 +25,31 @@ def history():
 
 @app.route('/project',methods=['GET','POST'])
 def predict():
+
     if request.method=="POST":
-        data=request.get_json()
+
+        brand_name=request.form['brand_name']             #request.form to get data from user input
+        owner=int(request.form['owner'])
+        age=int(request.form['age'])                      #added int to change  type from str to int
+        power=int(request.form['power'])
+        kms_driven=int(request.form['kms_driven']) 
+
+        brand_dict={'TVS':1, 'Royal Enfield':2, 'Triumph':3, 'Yamaha':4, 'Honda':5, 'Hero':6,
+       'Bajaj':7, 'Suzuki':8, 'Benelli':9, 'KTM':10, 'Mahindra':11, 'Kawasaki':12,
+       'Ducati':13, 'Hyosung':14, 'Harley-Davidson':15, 'Jawa':16, 'BMW':17, 'Indian':18,
+       'Rajdoot':19, 'LML':20, 'Yezdi':21, 'MV':22, 'Ideal':23}
+        
+        brand_name=brand_dict.get(brand_name)  # to change type from str to int
+
+        # print([[brand_name,age,kms_driven,owner,power]])
+        # print("types of data:",type(brand_name),type(age),type(kms_driven),type(owner),type(power))
+        data=[[brand_name,owner,age,kms_driven,power]]
+        pred=model.predict(data)
+        print("prediction",pred)
+        return render_template('project.html',prediction=pred)
+
     
     return render_template('project.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
